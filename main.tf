038ce8a8cac0398cab0c13cb4f82df4abf42f33a
shpeeddemon <48887985+shpeeddemon@users.noreply.github.com>
2022-02-28 19:30:10 +0000

    Update main.tf
    add tags and comments

--- main.tf
+++ main.tf
@@ -21,50 +21,79 @@ resource "aws_vpc" "org1_vpc" {
 
 }
 
-
+##subnets
 resource "aws_subnet" "org1_public_subnet" {
   vpc_id            = aws_vpc.org1_vpc.id
   cidr_block        = "10.0.0.0/24"
   availability_zone = "us-east-1a"
+  tags = {
+    Name = "org1_public_subnet"
+  }
 
 }
 resource "aws_subnet" "org1_private_subnet" {
   vpc_id            = aws_vpc.org1_vpc.id
   cidr_block        = "10.0.1.0/24"
   availability_zone = "us-east-1a"
-
+  tags = {
+    Name = "org1_private_subnet"
+  }
 }
 
-
+## eips
 resource "aws_eip" "nat_eip" {
-  vpc      = true
+  vpc = true
 
 }
+resource "aws_eip" "org1_webserver" {
+  instance = aws_instance.org1_webserver.id
+  vpc      = true
+  tags = {
+    Name = "org1_webserver"
+  }
+
+}
+
+## gatewayes
 
 resource "aws_nat_gateway" "org1_nat_gateway" {
   allocation_id = aws_eip.nat_eip.id
   subnet_id     = aws_subnet.org1_public_subnet.id
-
+  tags = {
+    Name = "org1_nat_gateway"
+  }
 }
 
 resource "aws_internet_gateway" "org1_internet_gateway" {
   vpc_id = aws_vpc.org1_vpc.id
-
+  tags = {
+    Name = "org1_internet_gateway"
+  }
 }
+
+##route tables
 resource "aws_route_table" "org1_route_table" {
   vpc_id = aws_vpc.org1_vpc.id
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.org1_internet_gateway.id
   }
+  tags = {
+    Name = "org1_route_table"
+  }
 }
 resource "aws_route_table" "org1_prive_route_table" {
   vpc_id = aws_vpc.org1_vpc.id
-    route {
-  cidr_block = "0.0.0.0/0"
-  nat_gateway_id = aws_nat_gateway.org1_nat_gateway.id 
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.org1_nat_gateway.id
+  }
+  tags = {
+    Name = "org1_prive_route_table"
   }
 }
+
+## associations because aperntly  its not automatic #learnt the hard way
 resource "aws_route_table_association" "org1_association" {
   subnet_id      = aws_subnet.org1_public_subnet.id
   route_table_id = aws_route_table.org1_route_table.id
@@ -75,6 +104,8 @@ resource "aws_route_table_association" "org1_association_prive" {
   route_table_id = aws_route_table.org1_prive_route_table.id
 }
 
+
+## ACL
 resource "aws_network_acl" "allowall" {
   vpc_id = aws_vpc.org1_vpc.id
   egress {
@@ -94,7 +125,7 @@ resource "aws_network_acl" "allowall" {
     to_port    = 0
   }
 }
-
+## SG
 resource "aws_security_group" "allowall" {
   name   = "allow all will fix later"
   vpc_id = aws_vpc.org1_vpc.id
@@ -112,19 +143,18 @@ resource "aws_security_group" "allowall" {
   }
 
 }
-resource "aws_eip" "org1_webserver" {
-  instance = aws_instance.org1_webserver.id
-  vpc      = true
-
-}
 
 
 
+# key pair
 resource "aws_key_pair" "defualt" {
   key_name   = "Team_one"
-  public_key = "YOU NEED TO CREATE YOUR OWN KEY PAIR FOR YOUR AWS ACCOUNT"
+  public_key = " YOU WILL NEED TO MAKE YOUR OWN KEY PAIR"
 }
 
+
+
+## EC2
 resource "aws_instance" "org1_webserver" {
 
   ami                    = "ami-038b3df3312ddf25d"
@@ -132,7 +162,9 @@ resource "aws_instance" "org1_webserver" {
   key_name               = aws_key_pair.defualt.key_name
   vpc_security_group_ids = [aws_security_group.allowall.id]
   subnet_id              = aws_subnet.org1_public_subnet.id
-
+  tags = {
+    Name = "org1_webserver"
+  }
 }
 resource "aws_instance" "org1_back_server" {
 
@@ -141,9 +173,13 @@ resource "aws_instance" "org1_back_server" {
   key_name               = aws_key_pair.defualt.key_name
   vpc_security_group_ids = [aws_security_group.allowall.id]
   subnet_id              = aws_subnet.org1_private_subnet.id
-
+  tags = {
+    Name = "org1_back_server"
+  }
 }
 
+
+##print stuff if need
 output "public_id" {
   value = aws_eip.org1_webserver.id
 }
